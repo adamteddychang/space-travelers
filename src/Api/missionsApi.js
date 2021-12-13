13711bd11e74838ca8ea64172d913174a5162c92
adamteddychang <adamchang0725@gmail.com>
2021-12-13 14:05:45 +0100

    fixed linters

--- src/Api/missionsApi.js
+++ src/Api/missionsApi.js
@@ -1,9 +1,9 @@
-const missionURL = "https://api.spacexdata.com/v3/missions";
+const missionURL = 'https://api.spacexdata.com/v3/missions';
 
-export const fetchMissionsApi  = async () => {
+export const fetchMissionsApi = async () => {
   const request = await fetch(missionURL);
   const missions = await request.json();
   return missions;
-}
+};
 
-export default {fetchMissions};
+export default { fetchMissionsApi };
--- src/Redux/missions/missions.js
+++ src/Redux/missions/missions.js
@@ -1,6 +1,6 @@
-import apiCalls from '../../Api/missionsApi'
-const FetchMissions = 'missionStore/missionactions/fetchMissions';
+import * as apiCalls from '../../Api/missionsApi';
 
+const FetchMissions = 'missionStore/missionactions/fetchMissions';
 
 const initialMissionState = [{
   mission_id: '',
@@ -11,20 +11,20 @@ const initialMissionState = [{
 
 export const fetchAllMissions = () => async (dispatch) => {
   const apiData = await apiCalls.fetchMissionsApi();
-  const payload = Object.values(apiData).map((i)=>i);
+  const payload = Object.values(apiData).map((i) => i);
   dispatch({
-    type:FetchMissions,
+    type: FetchMissions,
     payload,
-  })
-}
+  });
+};
 
 const missionsReducer = (state = initialMissionState, action) => {
   switch (action.type) {
     case FetchMissions:
       return action.payload;
-      default:
-        return state;
+    default:
+      return state;
   }
-}
+};
 
 export default { missionsReducer };
