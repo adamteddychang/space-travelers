ab0e69f90a3fc7135c6825170303910c83c4d8fe
luftedar <ugurorcun91@gmail.com>
2021-12-13 14:22:37 +0300

    Added fetch AllRockets Actions Creator

--- src/Redux/rockets/rocket.js
+++ src/Redux/rockets/rocket.js
@@ -7,6 +7,15 @@ const initialRocketState = [{
   reserved: false,
 }];
 
+export const fetchAllRockets = () => async (dispatch) => {
+  const apiData = await apiCalls.fetchRocketsApi();
+  const payload = Object.values(apiData).map((i) => i);
+  dispatch({
+    type: FETCH_ROCKETS,
+    payload,
+  });
+};
+
 const rocketsReducer = (state = initialRocketState, action) => ([state, action.payload]);
 
 export default { rocketsReducer };
