35e4fbe733bcb9a1d145d118033a91b409b9dc25
adamteddychang <adamchang0725@gmail.com>
2021-12-14 14:58:36 +0100

    completed join mission function

--- src/Components/Mission.js
+++ src/Components/Mission.js
@@ -1,13 +1,40 @@
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { join_mission } from '../Redux/missions/missions';
+
 function Mission(props) {
   const { mission } = props;
+  const dispatch = useDispatch();
+  
+  const joinMission = () => {
+    dispatch(join_mission(mission.mission_id));   
+    
+  }
   return (
     <tr key={mission.mission_id}>
+      
       <td>{mission.mission_name}</td>
       <td>{mission.description}</td>
       <td>Active</td>
-      <td>Join</td>
+      
+      <td>
+        <div>
+        {mission.joined === false || mission.joined === undefined
+        ? <button type="button" className="Join" onClick={joinMission}>Join</button>
+        : <button type="button" className="Leave">Leave</button>
+    }
+    </div>
+      </td>
     </tr>
   );
 }
+
+Mission.propTypes = {
+  mission: PropTypes.shape({
+    mission_name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
 
 export default Mission;
--- src/Redux/missions/missions.js
+++ src/Redux/missions/missions.js
@@ -1,6 +1,7 @@
 import * as apiCalls from '../../Api/missionsApi';
 
 const FETCH_MISSIONS = 'missionStore/missions/fetchMissions';
+const JOIN_MISSION = 'missionStore/missions/JOIN_MISSION'
 
 const initialMissionState = [{
   mission_id: '',
@@ -18,12 +19,27 @@ export const fetchAllMissions = () => async (dispatch) => {
   });
 };
 
+export const join_mission = (mission_id) =>({
+  type: JOIN_MISSION,
+  mission_id,
+});
+
+
 const missionsReducer = (state = initialMissionState, action) => {
   switch (action.type) {
     case FETCH_MISSIONS:
       return action.payload;
+
+    case JOIN_MISSION:
+      return state.map((mission)=>{
+        
+        if(mission.mission_id !== action.mission_id){ 
+        return mission;}
+        return { ...mission, joined:  true };
+      });
     default:
       return state;
+
   }
 };
 
