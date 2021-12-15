dcaf0c508bcf157d4a6e25edc28a9fcfb7dd52d0
adamteddychang <adamchang0725@gmail.com>
2021-12-15 11:37:44 +0100

    updated navbar css

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,9 +9,9 @@ function Navbar() {
         <h1 className="header-name">Space Travelers&apos Hub</h1>
       </div>
       <div className="right-nav">
-        <NavLink to="/" activeClassName="actived-navlink" exact>Rockets</NavLink>
-        <NavLink to="/missions" activeClassName="actived-navlink">Missions</NavLink>
-        <NavLink to="/myprofile" activeClassName="actived-navlink">My Profile</NavLink>
+        <NavLink to="/" activeClassName="actived-navlink" className="nav-links" exact>Rockets</NavLink>
+        <NavLink to="/missions" activeClassName="actived-navlink" className="nav-links">Missions</NavLink>
+        <NavLink to="/myprofile" activeClassName="actived-navlink" className="nav-links">My Profile</NavLink>
       </div>
     </nav>
   );
