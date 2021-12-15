86d46c511f4b521f4d751a8a695cb37c4a6c4615
adamteddychang <adamchang0725@gmail.com>
2021-12-15 10:13:09 +0100

    Added components snapshot testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import App from './App';
+import React from "react";
+import renderer from 'react-test-renderer';
+import { Provider } from "react-redux";
+import  store  from "./Redux/configureStore";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(
 
-    <Provider>
+const APP = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
 
-      <App />
-    </Provider>,
-  );
+describe('App Renders test', ()=>{
+  it('Renders App',()=>{
+    const app = renderer.create(APP).toJSON();
+    expect(app).toMatchSnapshot();
+  })
+})
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
-});
+
